95c86e14834a14f402af4caf91c9225965afdb46
João Piva <joao.piva@hub2b.com.br>
2023-10-24 15:31:52 -0300

    Commit inicial

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-2"
+  profile = "aws-pessoal"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-024e6efaf93d85776"
+  instance_type = "t2.micro"
+  key_name = "IaC Alura"
+  tags = {
+    Name = "Primeira instância IaC Alura"
+  }
+}
